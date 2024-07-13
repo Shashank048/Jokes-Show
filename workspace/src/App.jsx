d744b22299ea056647b5c0994b3b94e7d90fdfce
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-13 13:55:23 +0530

    Create App.jsx

---
+++ workspace/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Form from './Fom'
+import CommentsForm from './CommentsForm'
+import Counter from './Counter'
+import Joker from './Joker'
+
+function App() {
+  return (
+  <>
+  <Joker/>
+  </>
+  )
+}
+
+export default App
